6fca7ce1213164fbf78436430bfdaf1570a5d8c3
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-12-24 05:42:52 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 //your JS code here. If required.
-let counterValue = 0;
-
-	function handleIncrement() {
-            alert(`Current value before increment: ${counterValue}`);
-            counterValue++;
-            document.getElementById('counter').textContent = counterValue;
-        }
-
-        
-        document.getElementById('incrementBtn').addEventListener('click', handleIncrement);
 
