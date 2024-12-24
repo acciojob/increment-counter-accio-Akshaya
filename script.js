1425a74c9a55c8ea56529926c66720e8bf7990ed
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-12-24 00:07:26 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let counter = 0;
+const counter = document.getElementById("counter");
+const increment=document.getElementById("incrementBtn");
+
+incrementBtn.addEventListener('click',()=>{
+	counter++;
+	counter.innerHTML = counter;
+});
