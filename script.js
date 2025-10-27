dd7211d9fb96dcc3b65395ee416e06144ce92183
Akshaya <akshayaganesh0910@gmail.com>
2025-10-27 21:40:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If requireconst  
+const count = document.getElementById("counter");
+const IncrementButton = document.getElementById("incrementBtn");
+
+let count = 0;
+
+IncrementButton.addEventListener("click",()=>{
+	count++;
+	count.textContent = count;
+});
